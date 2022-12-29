cabc37d0e4b0d41c9f5db66bda8262d0cc35a4a1
abeerseada <121635964+abeerseada@users.noreply.github.com>
2022-12-29 20:50:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, w, n1, n2;
+
+
+            Console.Write("please enter the starting number of range: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("please enter theending number of range : ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The prime numbers between {0} and {1} are : \n", n1, n2);
+
+            for (x = n1; x <= n2; x++)
+            {
+                w = 0;
+
+                for (y = 2; y < x; y++)
+                {
+                    if (x % y == 0)
+                    {
+                        w++;
+                        break;
+                    }
+                }
+
+                if (w == 0 && x != 1)
+                    Console.Write("{0} ", x);
+            }
+            Console.Write("\n");
+        }
+    }
+}
+
